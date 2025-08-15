6038f5a3322235921e0b8435a2825403f86c800c
Brahillms <57116637+Brahillms@users.noreply.github.com>
2025-08-15 16:10:46 -0400

    New window rescaling implementation; Don't embed game on next play

--- scn/pixel_perfect_screen_scaling.gd
+++ scn/pixel_perfect_screen_scaling.gd
@@ -2,18 +2,26 @@ extends CanvasGroup
 
 # Deisgned to expand the game's internal small, fixed resolution for screens with much higher resolutions
 
-@export var pixel_perfect_scale_factor: int = 1
+@export var pixel_perfect_scale_factor: int = 1:
+	set(value):
+		pixel_perfect_scale_factor = value
+		_set_pixel_perfect_size()
 
 func _ready() -> void:
-	# Window size initialization
-	
-	ProjectSettings.set_setting("display/window/size/viewport_width", (256 * pixel_perfect_scale_factor))
-	ProjectSettings.set_setting("display/window/size/viewport_height", (192 * pixel_perfect_scale_factor))
+	_set_pixel_perfect_size()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _set_pixel_perfect_size() -> void:
 	%PixelPerfectScreenScaling.scale = Vector2i(pixel_perfect_scale_factor, pixel_perfect_scale_factor)
-	get_window().size = Vector2i((256 * pixel_perfect_scale_factor), (192 * pixel_perfect_scale_factor)) # Window's size should change when pixel_perfect_scale_factor changes value
+	get_window().size = Vector2i(
+	(256 * pixel_perfect_scale_factor), (192 * pixel_perfect_scale_factor)
+	)
+
+# In-game window rescaling
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("inc_screen_scale"):
+		if pixel_perfect_scale_factor <= 5:
+			pixel_perfect_scale_factor += 1
 	
-	if Input.is_action_just_pressed("ui_up"):
-		pixel_perfect_scale_factor += 1
+	if Input.is_action_just_pressed("dec_screen_scale"):
+		if pixel_perfect_scale_factor >= 2:
+			pixel_perfect_scale_factor -= 1
