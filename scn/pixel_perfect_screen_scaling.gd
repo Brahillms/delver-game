1d354b876e3c7c579da385469c4b8a7a872b4751
Brahillms <57116637+Brahillms@users.noreply.github.com>
2025-08-15 08:14:30 -0400

    CanvasGroup scale can be changed at runtime; bug with window resizing

---
+++ scn/pixel_perfect_screen_scaling.gd
@@ -0,0 +1,20 @@
+extends CanvasGroup
+
+# Deisgned to expand the game's internal small, fixed resolution for screens with much higher resolutions
+
+@export var pixel_perfect_scale_factor: int = 1
+
+func _ready() -> void:
+	# Window size initialization
+	
+	ProjectSettings.set_setting("display/window/size/viewport_width", (256 * pixel_perfect_scale_factor))
+	ProjectSettings.set_setting("display/window/size/viewport_height", (192 * pixel_perfect_scale_factor))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	%PixelPerfectScreenScaling.scale = Vector2i(pixel_perfect_scale_factor, pixel_perfect_scale_factor)
+	get_window().size = Vector2i((256 * pixel_perfect_scale_factor), (192 * pixel_perfect_scale_factor)) # Window's size should change when pixel_perfect_scale_factor changes value
+	
+	# Test of window's size dynamically changing
+	if Input.is_action_pressed("ui_up"):
+		pixel_perfect_scale_factor = 5 # Notice how the window's size should be changing because the get_window() method is called every frame.
