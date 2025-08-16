f3826fc2e0eeac185c73075b8921c898a16421fe
Brahillms <57116637+Brahillms@users.noreply.github.com>
2025-08-16 15:41:30 -0400

    Read a lot of documentation

--- scn/pixel_perfect_screen_scaling.gd
+++ scn/pixel_perfect_screen_scaling.gd
@@ -1,27 +1,27 @@
 extends CanvasGroup
 
-# Deisgned to expand the game's internal small, fixed resolution for screens with much higher resolutions
-
-var pixel_perfect_scale_factor: int = 1:
+var scale_factor: int = 1:
 	set(value):
-		pixel_perfect_scale_factor = value
+		scale_factor = value
 		_set_pixel_perfect_size()
+
+# Deisgned to expand the game's internal small, fixed resolution for screens with much higher resolutions
 
 func _ready() -> void:
 	_set_pixel_perfect_size()
 
 func _set_pixel_perfect_size() -> void:
-	%PixelPerfectScreenScaling.scale = Vector2i(pixel_perfect_scale_factor, pixel_perfect_scale_factor)
+	%PixelPerfectScreenScaling.scale = Vector2i(scale_factor, scale_factor)
 	get_window().size = Vector2i(
-	(256 * pixel_perfect_scale_factor), (192 * pixel_perfect_scale_factor)
+	(256 * scale_factor), (192 * scale_factor)
 	)
 
 # In-game window rescaling
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("inc_screen_scale"):
-		if pixel_perfect_scale_factor <= 5:
-			pixel_perfect_scale_factor += 1
+		if scale_factor <= 5:
+			scale_factor += 1
 	
 	if Input.is_action_just_pressed("dec_screen_scale"):
-		if pixel_perfect_scale_factor >= 2:
-			pixel_perfect_scale_factor -= 1
+		if scale_factor >= 2:
+			scale_factor -= 1
--- scn/player_move.gd
+++ scn/player_move.gd
@@ -1,19 +1,24 @@
+# Derived from Godot's generic player move script.
 extends CharacterBody2D
 
+@onready var pixel_perfect = get_node("../..")
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var SPEED: float
+var JUMP_VELOCITY: float
 
+func _ready() -> void:
+	SPEED = 100.0 * pixel_perfect.scale_factor
+	JUMP_VELOCITY = -280.0 * pixel_perfect.scale_factor
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -21,5 +26,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
