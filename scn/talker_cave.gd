6565035b857808e362385170b00ef2873c3bc5a7
Brahillms <57116637+Brahillms@users.noreply.github.com>
2025-08-27 17:48:03 -0400

    Enter cave dialogue

---
+++ scn/talker_cave.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+
+var is_within_talking_range: bool = false
+var shut_up: bool = false
+#var _reread_count: int = 0
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept") and is_within_talking_range and not shut_up:
+		shut_up = true
+		await %TextBoxManager.display_textbox(TextBoxManager.Init.OPENING, "player", "talking_looking_left", "This might be the only way in...")
+		await %TextBoxManager.display_textbox(TextBoxManager.Init.IDLE, "player", "talking_looking_left", "...or out.")
+		await %TextBoxManager.display_textbox(TextBoxManager.Init.CLOSING, "player", "pensive", "Please come home, buddy.")
+		Global.entered_cave = true
+
+
+func _on_talker_hitbox_manager_body_entered(_body: Node2D) -> void:
+	is_within_talking_range = true
+
+
+func _on_talker_hitbox_manager_body_exited(_body: Node2D) -> void:
+	is_within_talking_range = false
