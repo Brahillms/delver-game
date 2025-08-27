2db104348ce608c732261475586a20cacc257b15
Brahillms <57116637+Brahillms@users.noreply.github.com>
2025-08-26 21:41:14 -0400

    Mound of sand easter egg

--- scn/talker.gd
+++ scn/talker.gd
@@ -7,7 +7,8 @@ var shut_up: bool = false
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_within_talking_range and not shut_up:
 		shut_up = true
-		await %TextBoxManager.display_textbox(TextBoxManager.Init.OPENING_CLOSING, "", "", "")
+		await %TextBoxManager.display_textbox(TextBoxManager.Init.OPENING, "", "", "Inside of a small mound of sand...")
+		await %TextBoxManager.display_textbox(TextBoxManager.Init.CLOSING, "", "", "...you found absolutely nothing.")
 
 
 func _on_talker_hitbox_manager_body_entered(_body: Node2D) -> void:
