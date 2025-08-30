699c630041553cdf0444b69d9fc6c1799804d6bc
Brahillms <57116637+Brahillms@users.noreply.github.com>
2025-08-30 13:32:25 -0400

    Desert bg disappears when going into the cave

--- scn/desert_bg.gd
+++ scn/desert_bg.gd
@@ -6,9 +6,13 @@ extends ParallaxBackground
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	Global.entered_cave.connect(_on_entered_cave)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
 	transform = Transform2D(Vector2(pixel_perfect_screen_scaling.scale_factor, 0.0), Vector2(0.0, pixel_perfect_screen_scaling.scale_factor), Vector2.ZERO)
+
+
+func _on_entered_cave() -> void:
+	queue_free()
